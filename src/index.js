ea7ba09a11f0a8480f300eb813fc1147891026a4
v-duma <139060657+v-duma@users.noreply.github.com>
2023-12-11 00:46:41 +0200

    homework update

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ const main = document.querySelector('main');
 const form = document.querySelector('#search-form');
 const lightbox = new SimpleLightbox('.gallery a');
 let page = 1;
-
+let totalPages = 1; 
+console.log(totalPages);
 async function pageInfo(page) {
   const params = new URLSearchParams({
     per_page: 40,
@@ -22,6 +23,7 @@ async function pageInfo(page) {
   });
   const URL = `https://pixabay.com/api/?${params}`;
   const response = await axios.get(URL);
+  totalPages = Math.ceil(response.data.totalHits / 40);
   return response;
 }
 
@@ -29,34 +31,26 @@ async function sendRequest(event) {
   if (!form.elements.searchQuery.value.trim()) {
     return;
   }
-
   event.preventDefault();
   gallery.innerHTML = '';
+  page = 1;
 
   const response = await pageInfo(page);
   await imageProcessing(response.data.hits);
+
   if (response.data.totalHits === 0) {
     Notiflix.Notify.failure(
       "Sorry, we didn't find any images for your request"
     );
     loader.classList.remove('visible');
     main.style.marginTop = '0px';
-  }
-  if (response.data.totalHits === 1) {
+  } else if (response.data.totalHits === 1) {
     Notiflix.Notify.success(`Hooray! We found 1 image.`);
-  }
-  if (response.data.totalHits > 1) {
+  } else {
     Notiflix.Notify.success(
       `Hooray! We found ${response.data.totalHits} images.`
     );
   }
-  if (response.data.totalHits < 40 && response.data.totalHits !== 0) {
-    Notiflix.Notify.info(
-      `We're sorry, but you've reached the end of search results.`
-    );
-    loader.classList.remove('visible');
-  }
-  return (page = 1);
 }
 
 function imageProcessing(images) {
@@ -76,17 +70,17 @@ function imageProcessing(images) {
 
       return `
             <a class="gallery__link" href="${largeImageURL}">
-            <div class="gallery__item" id="${id}">
-            <img class="gallery-item_img" src="${webformatURL}" alt="${tags}" loading="lazy" />
-            <div class="info">
-            <p class="info-item"><b>Likes</b>${likes}</p>
-                        <p class="info-item"><b>Views</b>${views}</p>
-                        <p class="info-item"><b>Comments</b>${comments}</p>
-                        <p class="info-item"><b>Downloads</b>${downloads}</p>
-                        </div>
-                        </div>
-                        </a>
-                        `;
+              <div class="gallery__item" id="${id}">
+                <img class="gallery-item_img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+                <div class="info">
+                  <p class="info-item"><b>Likes</b>${likes}</p>
+                  <p class="info-item"><b>Views</b>${views}</p>
+                  <p class="info-item"><b>Comments</b>${comments}</p>
+                  <p class="info-item"><b>Downloads</b>${downloads}</p>
+                </div>
+              </div>
+            </a>
+          `;
     })
     .join('');
 
@@ -95,26 +89,22 @@ function imageProcessing(images) {
   loader.classList.add('visible');
 }
 
-form.addEventListener('submit', sendRequest);
-
 async function loadNextImages(entries, observer) {
   entries.forEach(async entry => {
-    if (entry.isIntersecting) {
+    if (entry.isIntersecting && page < totalPages) {
       page++;
       const response = await pageInfo(page);
       const images = response.data.hits;
       loader.classList.remove('visible');
+
       if (images.length === 0 && response.data.totalHits !== 0) {
         observer.unobserve(loader);
         Notiflix.Notify.info(
           `We're sorry, but you've reached the end of search results.`
         );
-        page = 1;
         return;
       }
-      if (page === 1 && gallery.innerHTML === '') {
-        return;
-      }
+
       imageProcessing(images);
 
       const { height: cardHeight } = document
@@ -131,3 +121,5 @@ async function loadNextImages(entries, observer) {
 
 const observer = new IntersectionObserver(loadNextImages);
 observer.observe(loader);
+
+form.addEventListener('submit', sendRequest);
