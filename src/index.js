406579ec2801cf91c200846ac3713d3de4d8cbda
v-duma <139060657+v-duma@users.noreply.github.com>
2023-12-11 14:54:35 +0200

    task done

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ const main = document.querySelector('main');
 const form = document.querySelector('#search-form');
 const lightbox = new SimpleLightbox('.gallery a');
 let page = 1;
+let totalPages;
+
+const distanceToBottom = 5;
 
 async function pageInfo(page) {
   const params = new URLSearchParams({
@@ -22,41 +25,35 @@ async function pageInfo(page) {
   });
   const URL = `https://pixabay.com/api/?${params}`;
   const response = await axios.get(URL);
+  totalPages = Math.ceil(response.data.totalHits / 40);
   return response;
 }
 
 async function sendRequest(event) {
+  observer.observe(loader);
   if (!form.elements.searchQuery.value.trim()) {
     return;
   }
-
   event.preventDefault();
   gallery.innerHTML = '';
+  page = 1;
 
   const response = await pageInfo(page);
   await imageProcessing(response.data.hits);
+
   if (response.data.totalHits === 0) {
     Notiflix.Notify.failure(
       "Sorry, we didn't find any images for your request"
     );
     loader.classList.remove('visible');
     main.style.marginTop = '0px';
-  }
-  if (response.data.totalHits === 1) {
+  } else if (response.data.totalHits === 1) {
     Notiflix.Notify.success(`Hooray! We found 1 image.`);
-  }
-  if (response.data.totalHits > 1) {
+  } else {
     Notiflix.Notify.success(
       `Hooray! We found ${response.data.totalHits} images.`
     );
   }
-  if (response.data.totalHits < 40 && response.data.totalHits !== 0) {
-    Notiflix.Notify.info(
-      `We're sorry, but you've reached the end of search results.`
-    );
-    loader.classList.remove('visible');
-  }
-  return (page = 1);
 }
 
 function imageProcessing(images) {
@@ -76,17 +73,17 @@ function imageProcessing(images) {
 
       return `
             <a class="gallery__link" href="${largeImageURL}">
-            <div class="gallery__item" id="${id}">
-            <img class="gallery-item_img" src="${webformatURL}" alt="${tags}" loading="lazy" />
-            <div class="info">
-            <p class="info-item"><b>Likes</b>${likes}</p>
-                        <p class="info-item"><b>Views</b>${views}</p>
-                        <p class="info-item"><b>Comments</b>${comments}</p>
-                        <p class="info-item"><b>Downloads</b>${downloads}</p>
-                        </div>
-                        </div>
-                        </a>
-                        `;
+              <div class="gallery__item" id="${id}">
+                <img class="gallery-item_img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+                <div class="info">
+                  <p class="info-item"><b>Likes</b>${likes}</p>
+                  <p class="info-item"><b>Views</b>${views}</p>
+                  <p class="info-item"><b>Comments</b>${comments}</p>
+                  <p class="info-item"><b>Downloads</b>${downloads}</p>
+                </div>
+              </div>
+            </a>
+          `;
     })
     .join('');
 
@@ -95,26 +92,14 @@ function imageProcessing(images) {
   loader.classList.add('visible');
 }
 
-form.addEventListener('submit', sendRequest);
-
 async function loadNextImages(entries, observer) {
   entries.forEach(async entry => {
-    if (entry.isIntersecting) {
+    if (entry.isIntersecting && page < totalPages) {
       page++;
       const response = await pageInfo(page);
       const images = response.data.hits;
       loader.classList.remove('visible');
-      if (images.length === 0 && response.data.totalHits !== 0) {
-        observer.unobserve(loader);
-        Notiflix.Notify.info(
-          `We're sorry, but you've reached the end of search results.`
-        );
-        page = 1;
-        return;
-      }
-      if (page === 1 && gallery.innerHTML === '') {
-        return;
-      }
+
       imageProcessing(images);
 
       const { height: cardHeight } = document
@@ -126,8 +111,14 @@ async function loadNextImages(entries, observer) {
         behavior: 'smooth',
       });
     }
+    if (totalPages === page) {
+      loader.classList.remove('visible');
+      Notiflix.Notify.info('You have reached the end of collection');
+      observer.unobserve(loader);
+      totalPages = null;
+    }
   });
 }
 
 const observer = new IntersectionObserver(loadNextImages);
-observer.observe(loader);
+form.addEventListener('submit', sendRequest);
